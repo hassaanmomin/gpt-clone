6cec2675c1d9fcf84d3ce62df96e1937c19fdc77
Hassaan <mhm_tanveer@hotmail.com>
2024-01-04 15:32:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       }
     }
     try {
-      const response = await fetch ('https://gpt-clone-one.vercel.app:8000/completions', options)
+      const response = await fetch ('http://localhost:8000/completions', options)
       const data = await response.json()
       setMessage(data.choices[0].message)
     } catch (err) {
